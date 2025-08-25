dc777a52fe4c0cf17b88c845b6300e80f0b535b0
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2025-08-25 19:42:32 +0200

    introduce AUTH_LOCAL_DISABLE

--- app/auth/decorator.py
+++ app/auth/decorator.py
@@ -1,9 +1,12 @@
+import os
 from functools import wraps
 from flask import session, redirect, url_for
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        if os.environ.get('AUTH_LOCAL_DISABLE'):
+            return f(*args, **kwargs)
         if 'user_id' not in session:
             return redirect(url_for('auth.login'))
         return f(*args, **kwargs)
